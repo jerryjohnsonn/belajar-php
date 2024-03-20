4f7e80c81d10d986b1e12b1ca590cc7c7c3cf550
Jerry Johnson <mail.jerryjohnson@gmail.com>
2024-03-20 21:17:06 +0700

    Basic PHP Learning Episode1

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php 
+
+// adasdf 
+// adfaas  
+// asdfasfasd
+
+// Standar Output
+// echo, print
+// print_r,
+// var_dump
+
+// Penulisan sintax PHP
+
+// Variabel dan tipe data di PHP
+// $nama = "RRQ Alberttt";
+
+// echo 'Halo nama saya adalah $nama';
+// echo "\nHalo nama saya adalah $nama";
+
+// Operator di PHP
+// Kurang lebih sama seperti operator yang ada di javascript
+
+// Operator concatenation string
+// $nama_depan = "Sandhika";
+// $nama_belakang = "Galih";
+
+// echo $nama_depan . ' ' . $nama_belakang
+
+// Assignment Operator (Operator Penugasan)
+
+// $x = 1;
+// $x += 5;
+// echo $x;
+
+// $nama = "Sandhika";
+// $nama .= " ";
+// $nama .= "Galih";
+
+// echo $nama;
+
+// Perbandingan, kurang lebih sama dengan kode yang ada di Javascript
+// var_dump(1 > 5);
+// var_dump(1 === "1");
+
+// Operator Logika, juga kurang lebih sama dengan operator yang ada biasanya. Operator logika kalau di javascript, !, &&, ||, kalau di javascript, juga begitu juga. Sama sih, engga ada bedanya...
+
+$x = 10;
+var_dump($x < 20 && $x % 2 == 0);
+
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Belajar PHP</title>
+</head>
+<body>
+  <h1>
+    Halo selamat datang <?php echo $nama ?>
+  </h1>
+</body>
+</html> -->
